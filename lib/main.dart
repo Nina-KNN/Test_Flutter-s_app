ed1b431e05e7b277efaf91222ff50a181378fb23
Nina <n.kruchinkina22022@gmail.com>
2020-01-28 22:43:55 +0200

    tapping anywhere on the screen a background color should be changed to a random color

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(MyApp());
 
@@ -6,7 +7,40 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hey there'),
+      home: RandomColor(),
+    );
+  }
+}
+
+class RandomColor extends StatefulWidget {
+  @override
+  RandomColorsState createState() => RandomColorsState();
+}
+
+class RandomColorsState extends State<RandomColor> {
+  Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: (){
+        setState(() { color = Color(Random().nextInt(0xffffffff)); });
+      },
+      child: Container(
+        child: Scaffold(
+          backgroundColor: color,
+          body: Center(
+            child: Text(
+              'Hey there',
+              style: TextStyle(
+                color: Colors.amberAccent,
+                fontSize: 32.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
